629f0ce0f7a158c1335f044f2b9e8d87d10d1518
lwt <874612642@qq.com>
2020-01-03 08:52:22 +0800

    xml整合mybatis和springboot

---
+++ src/main/java/com/lwt/springbootmybatisxml/mapper/AdminMapper.java
@@ -0,0 +1,11 @@
+package com.lwt.springbootmybatisxml.mapper;/*
+@author:liuwentao
+
+@create date:2020/1/2
+*/
+
+import com.lwt.springbootmybatisxml.bean.TAdmin;
+
+public interface AdminMapper {
+    TAdmin getTAdminById(Integer id);
+}
